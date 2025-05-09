af57ab2f869b39911a6659441f5dc11e0df49bbf
Fanis Khafizov <khafizovfanis@yandex.ru>
2025-05-09 21:51:09 +0300

    debug output

--- compressors.py
+++ compressors.py
@@ -51,14 +51,14 @@ class Compressor:
             criterion=criterion
         )
 
-        for name, param in self.model.named_parameters():
-            if not self.skip(name):
-                plt.hist(self.w[name].view(-1).cpu().numpy(), bins=100, alpha=0.5, label=name)
-                plt.title(f"Histogram of {name}")
-                plt.xlabel("Value")
-                plt.ylabel("Frequency")
-                plt.legend()
-                plt.show()
+        # for name, param in self.model.named_parameters():
+        #     if not self.skip(name):
+        #         plt.hist(self.w[name].view(-1).cpu().numpy(), bins=100, alpha=0.5, label=name)
+        #         plt.title(f"Histogram of {name}")
+        #         plt.xlabel("Value")
+        #         plt.ylabel("Frequency")
+        #         plt.legend()
+        #         plt.show()
 
     def compress(self, name, param):
         with torch.no_grad():
